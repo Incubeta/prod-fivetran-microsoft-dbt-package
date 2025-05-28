ac92d26b4156a6882d0a97f9a830861780ac7ae7
Miguelo Sana <miguelo@incubeta.com>
2025-05-28 19:17:19 +0200

    set model config

--- models/microsoft/microsoft-keyword_performance-v1.sql
+++ models/microsoft/microsoft-keyword_performance-v1.sql
@@ -1,8 +1,7 @@
 {{
   custom_config(
     alias=var('microsoft_keyword_performance_v1_alias','microsoft-keyword_performance-v1'),
-    field="TimePeriod",
-   event_time='TimePeriod')
+    field="TimePeriod")
 }}
 
 SELECT DISTINCT
